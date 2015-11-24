fe5676c0a1c3d0228e1099ec3b95a3195945bcf7
Christian Lüdemann <clu@it-minds.dk>
2015-11-24 14:48:56 +0100

    working on home button

--- IonicDemo/ng-app/Shared/Navbar/navbar.module.ts
+++ IonicDemo/ng-app/Shared/Navbar/navbar.module.ts
@@ -1,9 +1,5 @@
-﻿// Ionic Starter App
+﻿(function() {
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
-angular.module('navbar.module', ['core.module'])
+    angular.module('navbar.module', ['core.module'])
 
+})();
